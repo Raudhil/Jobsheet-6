83192ef187e45847bc82ba5b28c0d4ff8decdad0
Raudhil <raudhilfirdausn@gmail.com>
2023-10-03 09:21:09 +0700

    Raul3

---
+++ Pemilihan2Percobaan223.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan223{
+    public static void main(String[] args){
+        Scanner input23 = new Scanner(System.in);
+
+        int s1,s2,s3,totalSudut;
+
+        System.out.print("Masukkan sisi 1: ");
+        s1 = input23.nextInt();
+
+        System.out.print("Masukkan sisi 1: ");
+        s2 = input23.nextInt();
+
+        System.out.print("Masukkan sisi 1: ");
+        s3 = input23.nextInt();
+
+        totalSudut = s1 + s2 + s3;
+
+        if(totalSudut == 180){
+            if((s1 == 90) || (s2 == 90) || (s3 == 90))
+            System.out.println("Segitiga tersebut adalah segitiga siku - siku");
+            else
+            System.out.println("Segitiga tersebut bukan segitiga siku - siku");
+        } else
+            System.out.println("Bukan segitiga");
+    }
+}
