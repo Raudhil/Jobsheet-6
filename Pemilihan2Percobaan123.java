303cb073acfa9fe723391ad4e416db3d33243089
Raudhil <raudhilfirdausn@gmail.com>
2023-10-03 09:08:14 +0700

    Raul2

--- Pemilihan2Percobaan123.java
+++ Pemilihan2Percobaan123.java
@@ -9,8 +9,8 @@ public class Pemilihan2Percobaan123{
         System.out.print("Masukkan tahun: ");
         tahun = input23.nextInt();
 
-        if((tahun % 4) == 0){
-            if((tahun % 100) != 0){
+        if((tahun % 400) == 0){
+            if((tahun % 100) == 0){
                 System.out.println("Tahun Kabisat");
             }   else {
                 System.out.println("Bukan tahun kabisat");
